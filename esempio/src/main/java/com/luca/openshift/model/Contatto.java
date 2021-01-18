9cc298b970db50ee2eae13ec19b7d946afe16eca
kingo <kingo@DESKTOP-P0DINSI.lan>
2021-01-18 15:42:24 +0100

    progetto

---
+++ esempio/src/main/java/com/luca/openshift/model/Contatto.java
@@ -0,0 +1,44 @@
+package com.luca.openshift.model;
+
+import java.io.Serializable;
+
+public class Contatto implements Serializable {
+	
+	private static final long serialVersionUID = -7851383381691014732L;
+	private String nome;
+	private String cognome;
+	private String username;
+	public Contatto(String nome, String cognome, String username) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.username = username;
+	}
+	public Contatto() {
+		super();
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	@Override
+	public String toString() {
+		return "Contatto [nome=" + nome + ", cognome=" + cognome + ", username=" + username + "]";
+	}
+	
+	
+}
